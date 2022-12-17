f52906d0efb09513c5827d17c106848a64836e0c
Annika Kangur <annika_kangur@hotmail.com>
2022-12-17 16:36:59 +0200

    add week13 code

---
+++ Week13QLiteDB/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Data.SQLite;
+
+
+ReadData(CreateConnection());
+InsertCustomer(CreateConnection());
+//RemoveCustomer(CreateConnection());
+
+//CreateConnection();
+static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version=3; New = True; Compress = True");
+            try
+            {
+                connection.Open();
+                Console.WriteLine("DB found");
+            }
+            catch
+            {
+                Console.WriteLine("DB not found");
+            }
+            //connection.Close();
+            return connection;
+        }
+
+//ANDMETE MAHALUGEMINE
+        static void ReadData(SQLiteConnection myConnection)
+        {
+            Console.Clear();
+            SQLiteDataReader reader;
+            SQLiteCommand command;
+
+            command = myConnection.CreateCommand();
+            command.CommandText = "SELECT rowid, * FROM customer";
+            //command.CommandText = "SELECT customer.firstName, customer.lastname, status.statustype from customerStatus " +
+            //                      "FROM customerStatus " +
+            //                      "JOIN customer on customer.rowid = customerStatus.customerid " + 
+            //                      "JOIN status ON status.rowid = customerStatus.statusid";
+
+            reader = command.ExecuteReader();
+        
+
+    while (reader.Read())
+            {   
+
+                string readerRowId = reader["rowid"].ToString();
+                string readerStringFirstName = reader.GetString(1);
+                string readerStringLastName = reader.GetString(2);
+                string readerStringDob = reader.GetString(3);
+
+                Console.WriteLine($"{readerRowId}. Full name: {readerStringFirstName} {readerStringLastName}; DoB: {readerStringDob}");
+
+            }
+            myConnection.Close();
+        }
+
+//ANDMETE LISAMINE
+static void InsertCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+    string fName, lName, dob;
+
+    Console.WriteLine("Enter first name:");
+    fName = Console.ReadLine();
+    Console.WriteLine("Enter last name:");
+    lName = Console.ReadLine();
+    Console.WriteLine("Enter date of birth (mm-dd-yyyy):");
+    dob = Console.ReadLine();
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"INSERT INTO customer(firstName, lastName, dateOfBirth) " +
+    $"VALUES('{fName}', '{lName}', '{dob}')";
+
+    int rowInserted = command.ExecuteNonQuery();
+    Console.WriteLine($"Row inserted: {rowInserted}");
+
+    ReadData(myConnection);
+}
+
+//KASUTAJA KUSTUTAMINE
+//static void RemoveCustomer(SQLiteConnection myConnection)
+//{
+//    SQLiteCommand command;
+
+//    string idToDelete;
+//    Console.WriteLine("Enter an id to delete a customer:");
+//    idToDelete = Console.ReadLine();
+
+//    command = myConnection.CreateCommand();
+//    command.CommandText = $"DELETE FROM customer WHERE rowid = {idToDelete}";
+//    int rowRemoved = command.ExecuteNonQuery();
+//    Console.WriteLine($"{rowRemoved} was removed from the table customer.");
+
+//    ReadData(myConnection);
+
+//}
+
+//static void FindCustomer()
+//{
+
+//}
